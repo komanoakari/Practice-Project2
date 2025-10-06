92ce7e6b5c60f4447fe9884a3cb4f0d82cbab962
sekiakari <akr.60805@gmail.com>
2025-10-06 13:52:39 +0900

    ブランチ作成のみ

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\AttendanceController;
 
-Route::middleware('auth')->group(function() {
+
+
+Route::middleware(['auth','verified'])->group(function() {
     Route::get('/attendance', function() {
         return view('attendance.stamp');
     })->name('attendance.stamp');
@@ -13,3 +16,17 @@ Route::middleware('auth')->group(function() {
         return view('attendance.index');
     })->name('attendance.index');
 });
+
+Route::get('/email/verify', function() {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function(EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/attendance');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', '認証メールを再送信しました');
+})->middleware('auth')->name('verification.send');
