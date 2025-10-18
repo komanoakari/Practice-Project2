5e8017f21d888e3a58d601fc20c1194213ae27df
sekiakari <akr.60805@gmail.com>
2025-10-19 05:48:53 +0900

    Seederファイルの修正

--- src/database/factories/AttendanceFactory.php
+++ src/database/factories/AttendanceFactory.php
@@ -8,10 +8,7 @@ class AttendanceFactory extends Factory
 {
     public function definition(): array
     {
-        $date = $this->faker->dateTimeBetween('-1 month', 'now');
-
         return [
-            'date' => $date->format('Y-m-d'),
             'start_time' => '09:00:00',
             'end_time' => '18:00:00',
         ];
